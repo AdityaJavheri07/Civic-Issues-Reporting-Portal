65d67fa0ecad14e4fe4be8842f22f9644fc4cb44
AdityaJavheri07 <adityajavheri01@gmail.com>
2025-03-17 22:26:29 +0530

    Update predict.py
    intigrated pothole detection into main project

--- predict.py
+++ predict.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
@@ -9,13 +10,14 @@ def predict_image(img_path):
     img = image.load_img(img_path, target_size=(128, 128))
     img_array = image.img_to_array(img) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
-    
-    prediction = model.predict(img_array)
-    
-    if prediction[0][0] > 0.5:
-        print("Pothole detected!")
-    else:
-        print("No pothole detected.")
 
-# Test on a new image
-predict_image(r"C:\Users\Aditya\Downloads\images (1).jpg")
+    prediction = model.predict(img_array)
+
+    if prediction[0][0] > 0.5:
+        print("Pothole detected")
+    else:
+        print("No pothole detected")
+
+if __name__ == "__main__":
+    image_path = sys.argv[1]  # Get image path from command-line argument
+    predict_image(image_path)
